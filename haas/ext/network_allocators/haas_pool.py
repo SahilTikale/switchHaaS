cf0c3267824d1e58c04aa8d9abc6dbf24f276dd8
Sahil Tikale <sahil.tikale@gmail.com>
2015-09-05 02:24:19 -0400

    Implemented HaaS as a Network Allocator for Recursive HaaS:
    This commit consists of:
    
    1.) Generating a new net-id.
    Implementation logic differs significantly as compared to vlan_pools.
    Rather than fetch the ids from the table, we create random unique
    strings and populate the vlan table each time a new network_create
    request is encountered.
    
    2.) This string will be used as network-lable to be passed to Base HaaS
    once HaaS-as-a-Switch is done
    
    3.) Deleting the net-id
    Unlike vlan_pools, here the net-id is not released, rather the row is deleted
    whenever the respective network is deleted from rHaaS.
    
    PENDING:
    Need to discuss with George/Jason on what should be the legal/default channel
    for haas as a network allocator.

--- haas/ext/network_allocators/haas_pool.py
+++ haas/ext/network_allocators/haas_pool.py
@@ -11,30 +11,64 @@
 # IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 # express or implied.  See the License for the specific language
 # governing permissions and limitations under the License.
-"""A null network allocator.
+"""A network allocator for recursive HaaS
 
-Network IDs are random and arbitrary. The only supported channel is "null".
+Network IDs are generated at random and serve as network names
+for base HaaS
 """
-
+import logging
 import uuid
+
+from sqlalchemy import Column, String, Integer, Boolean
+
+from haas.config import cfg
+from haas import model
+from haas.model import AnonModel
 from haas.network_allocator import NetworkAllocator, set_network_allocator
 
 
-class NullNetworkAllocator(NetworkAllocator):
-    """A Null network allocator.
+db = model.Session()
 
-    Conforms to the interface specified for its superclass, NetworkAllocator.
+
+class HaasVlanAllocator(NetworkAllocator):
+    """An allocator of vlans to recursive HaaS.
+
+    The interface is as specified in ``NetworkAllocator``.
     """
-    def get_new_network_id(self, db):
-        return str(uuid.uuid1())
 
-    def free_network_id(self, db, net_id):
-        pass
+    def get_new_network_id(self, db):
+        """Creates a UUID for a new vlan and
+
+        1.) populate the haas_netid table in the database.
+        This string will be used as label to the base HaaS.
+        """
+        newNetid = "rhaas-"+str(uuid.uuid4())
+        add_netid = HaaS_Netid(net_id=newNetid)
+        db.add(add_netid)
+        db.commit()
+        return newNetid
+
+    def free_network_id(self, db, bhaas_netname):
+        bhaas_netName = db.query(HaaS_Netid).filter(HaaS_Netid.net_id==bhaas_netname).first()
+        if not bhaas_netName:
+            logger = logging.getLogger(__name__)
+            logger.error('vlan %s does not exist in database' % net_id)
+            return
+        db.delete(bhaas_netName)
+        db.commit()
 
     def populate(self, db):
+        """Recursive HaaS does not require this function.
+
+        Keeping it for compliance as a subclass.
+        """
         pass
 
     def legal_channels_for(self, db, net_id):
+        """Recursive Haas depends on Base HaaS for its values.
+
+        So it would be null in this case.
+        """
         return ["null"]
 
     def is_legal_channel_for(self, db, channel_id, net_id):
@@ -44,5 +78,16 @@ class NullNetworkAllocator(NetworkAllocator):
         return "null"
 
 
+class HaaS_Netid(AnonModel):
+    """This table will contain names of network in base HaaS
+
+    For every network created under recursive HaaS, it will generate
+    new network id in the form of a random string appended to "rhaas"
+    and store this string in this table
+    """
+
+    net_id = Column(String, nullable=False, unique=True)
+
+
 def setup(*args, **kwargs):
-    set_network_allocator(NullNetworkAllocator())
+    set_network_allocator(HaasVlanAllocator())
